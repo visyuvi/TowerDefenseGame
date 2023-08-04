1e6bda82718f000057556ae1509913f43cdb77e7
Your Name <you@example.com>
2023-08-04 16:10:32 +0530

    Code to animate the turrets

--- main.py
+++ main.py
@@ -22,6 +22,10 @@ placing_turrets = False
 # load images
 # map
 map_image = pg.image.load('levels/level.png').convert_alpha()
+
+# turret spritesheets
+turret_sheet = pg.image.load('assets/images/turrets/turret_1.png').convert_alpha()
+
 # individual turret image for mouse cursor
 cursor_turret = pg.image.load('assets/images/turrets/cursor_turret.png').convert_alpha()
 # enemies
@@ -50,7 +54,7 @@ def create_turret(mouse_position):
                 space_is_free = False
         # if it s a free space then create turret
         if space_is_free:
-            new_turret = Turret(cursor_turret, mouse_tile_x, mouse_tile_y)
+            new_turret = Turret(turret_sheet, mouse_tile_x, mouse_tile_y)
             turret_group.add(new_turret)
 
 
@@ -81,6 +85,7 @@ while run:
 
     # update groups
     enemy_group.update()
+    turret_group.update()
 
     ####################
     # DRAWING SECTION
